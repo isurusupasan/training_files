00976ad51af0135bf87d4d7da8a93fc2068fd46a
isurusupasan <isurusupasan@gmail.com>
2024-07-29 17:38:33 +0800

    add enumerator

--- prepare_trainig_file.py
+++ prepare_trainig_file.py
@@ -14,14 +14,21 @@ warnings.filterwarnings('ignore')
 
 # Manual check scores
 match_id = '20130607-M-Roland_Garros-SF-Novak_Djokovic-Rafael_Nadal'
-if os.path.exists('.json' % match_id):
-    file_name = '.json' % match_id
+if os.path.exists('%s.json' % match_id):
+    file_name = '%s.json' % match_id
 else:
     print("error importing")
     
 new_json_file = []
 json_file = json.load(open(file_name))
 
-print(json_file)
+#print(json_file)
+
+for clip_id, clip in enumerate(json_file):
+    if clip ['check'] == 'NA':
+        print(clip_id, clip)
+
+
+    
 
 
