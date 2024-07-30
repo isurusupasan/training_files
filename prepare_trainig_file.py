f808812bfa7eecc90836e2acd7f4d6b8c2c2063d
isurusupasan <isurusupasan@gmail.com>
2024-07-30 12:48:49 +0800

    write a new json file with NA events

--- prepare_trainig_file.py
+++ prepare_trainig_file.py
@@ -12,22 +12,51 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
+#add data into new json file
+def add_data(new_data, filename='training_data.json'):
+    #to check whether file is 
+    if os.path.exists(filename):
+        #load the existing data
+        with open(filename, 'r') as f:
+            try:
+                data=json.load(f)
+            except json.JSONDecodeError:
+                data = []
+    else:
+        #if the file not existed start with empty list
+        data = []
+
+    #append the new data
+    data.append(new_data)
+    
+    #write updated data back to the file
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2)
+
+    print("Data has been added to '{filename}'")
+
+
+
+
 # Manual check scores
 match_id = '20130607-M-Roland_Garros-SF-Novak_Djokovic-Rafael_Nadal'
 if os.path.exists('%s.json' % match_id):
     file_name = '%s.json' % match_id
 else:
     print("error importing")
-    
-new_json_file = []
+    #new_json_file = []
 json_file = json.load(open(file_name))
-
-#print(json_file)
 
 for clip_id, clip in enumerate(json_file):
     if clip ['check'] == 'NA':
         print(clip_id, clip)
+        add_data(clip)
+    
 
+print('Filtered data has been written to the jason file')
+
+
+        
 
     
 
