d8cd87ead51f212dfc43b6f8f672556d4071999a
isurusupasan <isurusupasan@gmail.com>
2024-07-31 18:36:24 +0800

    fine_label changed and delete the keys without label in the process

--- prepare_trainig_file.py
+++ prepare_trainig_file.py
@@ -53,20 +53,39 @@ def add_data(new_data, keys_to_keep, filename='training_data.json'):
     print("Data has been added to '{filename}'")
 
 # # to change the fine-label into label
-# def rename(data, old_key, new_key):
-#     if isinstance(data, dict):
-#         #print('b')
-#         for key in list(data.keys()):
-#             print(key)
-#             if key == old_key:
-#                 #print('a')
-#                 data[new_key]=data.pop(old_key)
-#                 rename(data[new_key], old_key, new_key)
-#     elif isinstance(data, list):
-#         for item in data:
-#             #print(item)
-#             rename(item, old_key, new_key)
-                    
+def rename(data, old_key, new_key):
+    
+    if isinstance(data, dict):
+        # print('a')
+        # print('----------------------')
+        #to delete the frames without labels
+        for key in list(data.keys()):
+            if data.fromkeys(old_key) is None or old_key=="":
+                del data[key]
+            # print('b')
+            # print(data.values())
+            # if key == "frame":
+            #     # print(data.fromkeys("frame"))
+            #     # if data[old_key] is None or data[old_key] == "":
+            #     #      del data[key]
+            if key == old_key:
+                # print(old_key)
+                # print(new_key)
+                # print('c')
+                data[new_key]=data.pop(old_key)
+                # print('d')
+                # print(key)
+
+            else:
+                rename(data[key], old_key, new_key)
+            # print('e')
+                
+    elif isinstance(data, list):
+        for item in data:
+            # print('f')
+            rename(item, old_key, new_key)
+            # print('g')
+    return data                
 
 # upload the json into code
 match_id = '20130607-M-Roland_Garros-SF-Novak_Djokovic-Rafael_Nadal'
@@ -77,17 +96,17 @@ else:
     #new_json_file = []
 json_file = json.load(open(file_name))
 # check new method
-# updated_json = rename(json_file, "video", "sq_img")
+updated_json = rename(json_file, "fine_label", "label")
 # print(updated_json)
 
-for clip_id, clip in enumerate(json_file): 
+for clip_id, clip in enumerate(updated_json): 
     if clip ['check'] == 'NA':
         print(clip_id, clip)
-        keys_to_keep = ["events", "frame", "fine_label", "check", "fps", "num_frames", "video", "far_name", "far_hand", "far_set", "far_game", "far_point", "near_name", "near_hand", "near_set", "near_game", "near_point"]
+        keys_to_keep = ["events", "frame", "label", "check", "fps", "num_frames", "video", "far_name", "far_hand", "far_set", "far_game", "far_point", "near_name", "near_hand", "near_set", "near_game", "near_point"]
         add_data(clip, keys_to_keep)
 
 
-print('Filtered data has been written to the jason file')
+# print('Filtered data has been written to the jason file')
 
 
         
